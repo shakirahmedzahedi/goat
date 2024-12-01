cab17113fe2fbad66d4202d9c4c1f1705131eeb2
Shakir Ahmed Zahedi <shakirahmed84@yahoo.com>
2024-12-01 11:03:45 +0100

    new comit

--- src/main/java/com/saz/se/goat/auth/AuthenticationController.java
+++ src/main/java/com/saz/se/goat/auth/AuthenticationController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/auth")
 public class AuthenticationController {
 
